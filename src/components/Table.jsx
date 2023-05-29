4df99d95106c554de69fcf175da569c4262a62ab
jhonlloydbabon <jlbabon12@gmail.com>
2023-05-29 11:53:19 +0100

    add moment in table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React,{useState} from 'react';
 import {AiFillEdit, AiOutlineFolderView} from 'react-icons/ai';
 import Modal from './UpdateAdminModal';
+import moment from 'moment/moment';
 
 function Table({tableHeaders, results, search, currentPage }) {
     const [ updateModel, setUpdateModal] = useState(false);
@@ -73,20 +74,20 @@ setUpdateModal(true);
                     results
                     .map((result)=>(
                         <tr className='' key={result.patientId}>
-                            <td className='p-2 text-center'>
+                            <td className='p-2 flex justify-center  '>
                                 <img src={result.profile} className=' w-11 h-11 rounded-full border border-gray-400 border-2 ' alt="User" />
                             </td>
                             <td className=' text-center capitalize '>
                                 {`${result.firstname} ${result.middlename !== "" ? result.middlename.charAt(0).concat(".") : '' } ${result.lastname}`}
                             </td>
                             <td className='text-center'>
-                                {result.birthday}
+                                {moment(result.birthday).format(`LL`)}
                             </td>
                             <td className='text-center capitalize'>
                                 {result.gender}
                             </td>
                             <td className='text-center'>
-                                {result.phoneNumber}
+                                {result.contactNumber}
                             </td>
                             <td className='text-center'>
                                 {result.email}
@@ -95,15 +96,22 @@ setUpdateModal(true);
                                 {result.verified ? 
                                     <p className=' px-2 py-2 rounded-md bg-cyan-500 text-white cursor-pointer hover:shadow-md ' onClick={()=>disableAccountBtn(result.patientId, false)}>Active</p> : 
                                     <p className=' px-2 py-2 rounded-md bg-red-500 text-white cursor-pointer hover:shadow-md '  onClick={()=>disableAccountBtn(result.patientId, true)}>Inactive</p> 
-                                }
+                                }   
                             </td>
-                            <td className=' mt-2 flex justify-center gap-x-1 '>
-                                <button className='bg-blue-500 text-white flex items-center px-4 py-2 rounded-md' onClick={()=>update(result.patientId)}>
-                                    <AiFillEdit size={25} />&nbsp;Update
-                                </button>
-                                <button className='bg-gray-500 text-white flex items-center px-4 py-2 rounded-md'>
-                                    <AiOutlineFolderView size={25} />&nbsp;View
-                                </button>
+                            <td className=' h-auto relative bottom-2 w-auto flex items-start justify-center gap-3'>
+                                <p className=' px-5 py-2 rounded-md bg-blue-500 text-white cursor-pointer hover:shadow-md flex' onClick={()=>update(
+                                    result.patientId,
+                                    result.firstname,
+                                    result.middlename,
+                                    result.lastname,
+                                    result.address,
+                                    result.birthday,
+                                    result.email,
+                                    result.gender,
+                                    result.contactNumber,
+                                    result.profile
+                                )}><AiFillEdit size={25} />&nbsp;Update</p>
+                                <p className=' px-5 py-2 rounded-md bg-gray-500 text-white cursor-pointer hover:shadow-md flex' onClick={()=>update(result.patientId)}><AiOutlineFolderView size={25} />&nbsp;View</p> 
                             </td>
                         </tr> ))
                     : results
@@ -118,9 +126,9 @@ setUpdateModal(true);
                                 {`${result.firstname} ${result.middlename !== "" ? result.middlename.charAt(0).concat(".") : '' } ${result.lastname}`}
                             </td>
                             <td className='text-center'>
-                                {result.birthday}
+                                {moment(result.birthday).format(`LL`)}
                             </td>
-                            <td className='text-center'>
+                            <td className='text-center capitalize'>
                                 {result.gender}
                             </td>
                             <td className='text-center'>
