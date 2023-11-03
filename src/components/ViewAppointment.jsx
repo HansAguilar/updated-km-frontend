b1e23faf250337cf26ef4b309a91efa70e25d25d
HansAguilar <aguilarhans100701@gmail.com>
2023-11-03 12:02:49 +0800

    QR Pages changed

--- src/components/ViewAppointment.jsx
+++ src/components/ViewAppointment.jsx
@@ -1,92 +1,91 @@
-import React,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import QRCode from 'qrcode';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-function ViewAppointment({view,setView}) {
+function ViewAppointment({ view, setView }) {
   const { id } = useParams();
-  const info = useSelector((state)=>state.appointment.payload.filter(val=>val.appointmentId===id));
+  const info = useSelector((state) => state.appointment.payload.filter(val => val.appointmentId === id));
   const details = [...info];
   const [qrcode, setQrcode] = useState("");
 
-  const generateToQrCode = () =>{
+  const generateToQrCode = () => {
     QRCode.toDataURL(`${details.appointmentId}`, {
       width: 400,
       margin: 2,
-    }, (err,url)=>{
-      if(err) return console.error(err);
+    }, (err, url) => {
+      if (err) return console.error(err);
       setQrcode(url);
     })
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     generateToQrCode();
-  },[id]);
-  
+  }, [id]);
+
   return (
     <div
-    className={` w-full h-screen bg-gray-900 bg-opacity-75 absolute top-0 left-0 z-40 flex justify-center items-center ${
-      view.isShow ? '' : 'hidden'
-    }`}
-  >
-    <div className=' z-50 h-auto relative'>
-      <div className='m-auto w-[900px] h-[750px] bg-white overflow-auto p-8 rounded-lg relative shadow-lg'>
-        <div className=' w-full text-left py-4 h-auto overflow-auto'>
-          <h2 className='text-2xl font-bold mb-2 text-gray-700 '>Appointment Information</h2>
-          <hr />
-          <br />
-          <form className=' w-full flex gap-2 '>
-            <div className=' w-80 p-4 h-auto font-semibold shadow-md bg-cyan-600 rounded-md flex flex-col items-center gap-2 '>
+      className={` w-full h-screen bg-gray-900 bg-opacity-75 absolute top-0 left-0 z-40 flex justify-center items-center ${view.isShow ? '' : 'hidden'
+        }`}
+    >
+      <div className=' z-50 h-auto relative'>
+        <div className='m-auto w-[900px] h-[750px] bg-white overflow-auto p-8 rounded-lg relative shadow-lg'>
+          <div className=' w-full text-left py-4 h-auto overflow-auto'>
+            <h2 className='text-2xl font-bold mb-2 text-gray-700 '>Appointment Information</h2>
+            <hr />
+            <br />
+            <form className=' w-full flex gap-2 '>
+              <div className=' w-80 p-4 h-auto font-semibold shadow-md bg-cyan-600 rounded-md flex flex-col items-center gap-2 '>
                 <img src={details.patient?.profile} alt="patient profile" className=' w-36 h-36 rounded-full border border-2 border-gray-300 ' />
                 <div className=' text-white text-xl mt-1 font-normal '>
                   <p className=' uppercase ' >
-                    {details.patient?.firstname} {details.patient?.middlename ? details.patient?.middlename.charAt(0).concat("."): ""} {details.patient?.lastname}
-                    </p>
-                    <p className=' text-center mt-1 text-gray-300 text-xs font-bold'>
+                    {details.patient?.firstname} {details.patient?.middlename ? details.patient?.middlename.charAt(0).concat(".") : ""} {details.patient?.lastname}
+                  </p>
+                  <p className=' text-center mt-1 text-gray-300 text-xs font-bold'>
                     Patient
-                    </p>
+                  </p>
                 </div>
                 <div className=' w-full mt-2 p-2 border border-b-0 border-l-0 border-r-0 border-t-white '>
-                    <p className=' text-white flex justify-between '>Gender <span className=' text-gray-200 capitalize ' >{details.patient?.gender}</span></p>
+                  <p className=' text-white flex justify-between '>Gender <span className=' text-gray-200 capitalize ' >{details.patient?.gender}</span></p>
                 </div>
                 <div className=' w-full p-2 border border-b-0 border-l-0 border-r-0 border-t-white '>
-                    <p className=' text-white flex justify-between '>Birthdate <span className=' text-gray-200 capitalize ' >{details.patient?.birthday}</span></p>
+                  <p className=' text-white flex justify-between '>Birthdate <span className=' text-gray-200 capitalize ' >{details.patient?.birthday}</span></p>
                 </div>
                 <div className=' w-full p-2 border border-b-0 border-l-0 border-r-0 border-t-white '>
-                    <p className=' text-white flex justify-between '>Age<span className=' text-gray-200 capitalize ' >{details.patient?.age}</span></p>
+                  <p className=' text-white flex justify-between '>Age<span className=' text-gray-200 capitalize ' >{details.patient?.age}</span></p>
                 </div>
                 <div className=' w-full p-2 border border-b-0 border-l-0 border-r-0 border-t-white '>
-                    <p className=' text-white flex justify-between '>Phone <span className=' text-gray-200 capitalize ' >{details.patient?.contactNumber}</span></p>
+                  <p className=' text-white flex justify-between '>Phone <span className=' text-gray-200 capitalize ' >{details.patient?.contactNumber}</span></p>
                 </div>
                 <div className=' w-full p-2 border border-l-0 border-r-0 border-t-white '>
-                    <p className=' text-white flex justify-between '>Email <span className=' text-gray-200 ' >{details.patient?.email}</span></p>
+                  <p className=' text-white flex justify-between '>Email <span className=' text-gray-200 ' >{details.patient?.email}</span></p>
                 </div>
 
                 <div className=' mt-3 flex flex-col justify-center gap-y-3 '>
                   {
                     qrcode && (
-                     <>
-                          <img src={qrcode} alt="qrcode" className=' rounded-xl ' />
-                       <a href={qrcode} download={`${details.patient?.firstname} ${details.patient?.middlename ? details.patient?.middlename.charAt(0).concat("."): ""} ${details.patient?.lastname}-appointment-qrcode.png`} 
-                       className=' text-white text-center uppercase '
-                       >Download</a>
-                     </>
+                      <>
+                        <img src={qrcode} alt="qrcode" className=' rounded-xl ' />
+                        <a href={qrcode} download={`${details.patient?.firstname} ${details.patient?.middlename ? details.patient?.middlename.charAt(0).concat(".") : ""} ${details.patient?.lastname}-appointment-qrcode.png`}
+                          className=' text-white text-center uppercase '
+                        >Download</a>
+                      </>
                     )
                   }
                 </div>
-            </div>
+              </div>
 
-            <div className=' flex-grow font-semibold text-green-700 '>
-            dw
-            </div>
-          </form>
-          <button onClick={()=>setView({
-            ...view,
-            isShow: false,
-          })}>Close</button>
+              <div className=' flex-grow font-semibold text-green-700 '>
+                dw
+              </div>
+            </form>
+            <button onClick={() => setView({
+              ...view,
+              isShow: false,
+            })}>Close</button>
+          </div>
         </div>
-       </div>
-    </div>
+      </div>
     </div>
   )
 }
--- src/pages/QRScanPage.js
+++ src/pages/QRScanPage.js
@@ -6,7 +6,7 @@ import { ToastContainer } from 'react-toastify';
 import { approvedPayment } from "../redux/action/PaymentAction";
 import { acceptAppointment } from "../redux/action/AppointmentAction";
 import moment from 'moment';
-
+import appointmentBg from "../assets/appointmentBg.png";
 
 export default function QRScanPage() {
   const { id } = useParams();
@@ -30,31 +30,33 @@ export default function QRScanPage() {
 
   console.log(payment);
 
-  // const checkIfExistingAppointment = () => {
-  //   const appointmentTime = moment(appointment[0].timeStart, "HH:mm:00").add(10, "minutes");
-  //   const appointmentDate = moment(appointment[0].appointmentDate).format("YYYY-MM-DD");
-  //   setAppointmentDetails(...appointment);
-  //   toastHandler("success", "Successfully scan the QR");
-  // }
-
   const checkIfExistingAppointment = () => {
-    if (appointment.length > 0) {
-      const appointmentTime = moment(appointment[0].timeStart, "HH:mm:00").add(10, "minutes");
-      const appointmentDate = moment(appointment[0].appointmentDate).format("YYYY-MM-DD");
-      console.log(todaysDate.toString() === appointmentDate.toString() && todaysTime.isBefore(appointmentTime));
-      if ((todaysDate.toString() === appointmentDate.toString() && todaysTime.isBefore(appointmentTime))) {
-        setAppointmentDetails(...appointment);
-        toastHandler("success", "Successfully scan the QR");
-      } else {
-        toastHandler("error", "Invalid Appointment");
-        navigate(-1);
-      }
-    }
-    else if (appointment.length < 1 || appointment === null) {
-      toastHandler("error", "Appointment not found");
-      navigate("/admin/dashboard/");
-    }
+    const appointmentTime = moment(appointment[0].timeStart, "HH:mm:00").add(10, "minutes");
+    const appointmentDate = moment(appointment[0].appointmentDate).format("YYYY-MM-DD");
+    setAppointmentDetails(...appointment);
+    toastHandler("success", "Successfully scan the QR");
+
+    console.log("Appoiontment details: " + appointmentDetails);
   }
+
+  // const checkIfExistingAppointment = () => {
+  //   if (appointment.length > 0) {
+  //     const appointmentTime = moment(appointment[0].timeStart, "HH:mm:00").add(10, "minutes");
+  //     const appointmentDate = moment(appointment[0].appointmentDate).format("YYYY-MM-DD");
+  //     console.log(todaysDate.toString() === appointmentDate.toString() && todaysTime.isBefore(appointmentTime));
+  //     if ((todaysDate.toString() === appointmentDate.toString() && todaysTime.isBefore(appointmentTime))) {
+  //       setAppointmentDetails(...appointment);
+  //       toastHandler("success", "Successfully scan the QR");
+  //     } else {
+  //       toastHandler("error", "Invalid Appointment");
+  //       navigate(-1);
+  //     }
+  //   }
+  //   else if (appointment.length < 1 || appointment === null) {
+  //     toastHandler("error", "Appointment not found");
+  //     navigate("/admin/dashboard/");
+  //   }
+  // }
   useEffect(() => {
     checkIfExistingAppointment();
   }, [id]);
@@ -80,37 +82,54 @@ export default function QRScanPage() {
     window.location.reload(navigate("/admin/dashboard/"));
   }
   return appointmentDetails && (
-    <div className='h-screen w-[800px] m-auto mt-32 overflow-hidden relative bg-gray-200 '>
+    <div className='h-screen w-full m-auto flex gap-2 items-center justify-center overflow-hidden relative flex-col'>
       <ToastContainer limit={1} autoClose={1500} />
-      <div className=' w-auto h-auto bg-white p-5 rounded-md '>
 
-        <div className=' w-full h-auto '>
-          <h3 className=' w-full py-3 border-b-2 font-bold text-lg text-zinc-700 '>Patient Profile</h3>
-          <div className=' pl-5 py-5 flex flex-grow gap-2 '>
-            <img src={`${appointmentDetails.patient.profile}`} className=' w-44 h-44  ' alt='Patient Profile' />
-            <div className=' w-72 '>
-              <h3 className=' text-md font-bold text-zinc-600 '>{appointmentDetails.patient.firstname} {appointmentDetails.patient.middlename ? `${appointmentDetails.patient.middlename}.` : ''} {appointmentDetails.patient.lastname}</h3>
-              <h4 className=' text-sm text-zinc-500 '>{moment(appointmentDetails.appointmentDate).format("dddd, MMM DD YYYY")} <span className=' text-sm text-cyan-500 '>({moment(appointmentDetails.timeStart, "HH:mm").format("LT")} - {moment(appointmentDetails.timeEnd, "HH:mm").format("LT")})</span></h4>
-              <div className={`${appointmentDetails.status === "PENDING" ? " bg-orange-400" : "bg-emerald-500"} text-white text-xs px-5 mt-1 py-[2px]  w-fit uppercase rounded-full `}>{appointmentDetails.status.toLowerCase()}</div>
+      <div className='w-[700px] h-[620px] bg-white p-4 rounded relative shadow'>
+        <img src={appointmentBg} className='absolute top-0 left-0 h-48 w-full z-0 rounded' />
+
+        <div className='w-full relative z-0 text-center'>
+          <img src={`${appointmentDetails.patient.profile}`} className='rounded-lg m-auto w-36 h-36 aspect-auto' alt='Patient Profile' />
+          <h3 className='font-medium text-white uppercase'>{appointmentDetails.patient.firstname} {appointmentDetails.patient.middlename ? `${appointmentDetails.patient.middlename}.` : ''} {appointmentDetails.patient.lastname}</h3>
+        </div>
+
+        <div className='mt-5 w-full p-5 flex flex-col gap-3'>
+          <div className='flex flex-col gap-2 border-b-2 pb-2 border-slate-300'>
+            <div className='flex justify-between items-center'>
+              <h1 className='font-semibold text-xl text-slate-700 tracking-wide'>Appointment Information</h1>
+              <div className={`${appointmentDetails.status === "PENDING" ? "bg-yellow-100 text-yellow-600" : "bg-green-100 text-green-600"} transition-all ease-linear duration-150 py-1 px-3 rounded-full text-sm`}>{appointmentDetails.status.charAt(0).toUpperCase() + appointmentDetails.status.substring(1).toLowerCase()}</div>
+            </div>
+            <div className='flex flex-col gap-2'>
+              <div className='flex justify-between items-center'>
+                <p className='text-slate-500 font-medium'>Date:</p>
+                <p className='text-slate-700 font-medium'>{moment(appointmentDetails.appointmentDate).format("dddd, MMM DD YYYY")}</p>
+              </div>
+              <div className='flex justify-between items-center'>
+                <p className='text-slate-500 font-medium'>Time:</p>
+                <p className='text-slate-700 font-medium'>{moment(appointmentDetails.timeStart, "HH:mm").format("LT")} - {moment(appointmentDetails.timeEnd, "HH:mm").format("LT")}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-2'>
+            <div className='flex items-center justify-between'>
+              <h1 className='font-semibold text-xl text-slate-700 tracking-wide'>Payment Information</h1>
+            </div>
+
+            <div className='flex justify-between items-center'>
+              <p className='text-slate-500 font-medium'>Status:</p>
+              <p className={`text-sm font-medium text-white py-1 px-3 rounded-full ${payment[0]?.status === "APPROVED" ? "bg-green-100 text-green-600 cursor-default" : "bg-yellow-100 text-yellow-600 hover:bg-yellow-400 hover:text-white cursor-pointer"}`} onClick={approvedPaymentButton}>{payment[0]?.status === "PENDING" ? "Accept Payment" : payment[0]?.status.toLowerCase()}</p>
+            </div>
+
+            <div className='flex flex-col flex-grow gap-2 '>
+              <p className=' w-full capitalize flex justify-between items-start '><span className='text-slate-500 font-medium'>Payment Method:</span> {payment[0]?.method}</p>
+              <p className=' w-full capitalize flex justify-between items-start '><span className='text-slate-500 font-medium'>Payment type:</span> {payment[0]?.type}</p>
+              <p className='bg-slate-200/70 rounded p-2 w-full capitalize flex justify-between items-start text-lg font-semibold mt-2'><span className='text-slate-500'>Total Payment:</span> ₱ {payment[0]?.totalPayment.toLocaleString()}</p>
             </div>
           </div>
         </div>
 
-        <div className=' w-full h-auto '>
-          <div className=' w-full h-auto flex justify-between items-center py-3 border-b-2 font-bold text-lg text-zinc-700 '>
-            <h3>Payment Details </h3>
-
-            <button onClick={approvedPaymentButton} className={`px-5 py-1 ${payment[0]?.status === "APPROVED" ? "bg-emerald-400 cursor-default" : "bg-orange-400 cursor-pointer"} capitalize rounded-lg text-white mt-3 font-medium text-sm `}>
-              {payment[0]?.status === "PENDING" ? "Accept Payment" : payment[0]?.status.toLowerCase()}</button>
-          </div>
-          <div className=' pl-5 py-5 flex flex-col flex-grow gap-2 '>
-            <p className=' w-full capitalize flex justify-between items-start '><span className=' text-zinc-600 font-bold '>Payment Method:</span> {payment[0]?.method}</p>
-            <p className=' w-full capitalize flex justify-between items-start '><span className=' text-zinc-600 font-bold '>Payment type:</span> {payment[0]?.type}</p>
-            <p className=' w-full capitalize flex justify-between items-start '><span className=' text-zinc-600 font-bold '>Total Payment:</span> ₱ {payment[0]?.totalPayment.toLocaleString()}</p>
-          </div>
-        </div>
-
-        <button onClick={btnSubmitAppointment} className=' w-full py-3 bg-cyan-500 text-white rounded-md '>Done</button>
+        <button onClick={btnSubmitAppointment} className='uppercase w-full p-4 bg-blue-500 font-semibold tracking-wide text-white rounded hover:bg-blue-700'>CONFIRM</button>
       </div>
     </div>
   )
